ce0bc0d42e965111eba32f21fa76019fa19d3ca7
Egg Targaryen <dzxrly2@gmail.com>
2024-08-11 11:49:26 +0800

    :sparkles: 引入了操作系统信息API

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ def get_network_state():
         abort(400)
 
 
-@app.route('./os_info', methods=['GET'])
+@app.route('/os_info', methods=['GET'])
 def get_os_info():
     try:
         return jsonify(get_operating_system_info())
